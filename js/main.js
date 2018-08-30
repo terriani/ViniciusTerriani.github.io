dc0b8b177968579bc61e145b2a357259c224a858
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-29 23:13:40 -0300

    corrigindo transição de cores em celulres

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ if(height > width){
     }else{
       $('.btn-up').fadeOut();
     }
-    if($(document).scrollTop() > 500){
+    if($(document).scrollTop() > 550){
       $(".main").css("background-color", "#333");
       $(".main").css("transition", ".2s");
       $(".main").css("color", "#f3f3f3");
