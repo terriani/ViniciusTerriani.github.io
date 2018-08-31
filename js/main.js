33e3921a1abb33f3ef7be4c364b59f71a03ca079
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-31 00:40:05 -0300

    corrigido formulario em landscapr

--- js/main.js
+++ js/main.js
@@ -217,17 +217,17 @@ if(height > width){
       project_area.css("opacity", 1);
       project_area.css("transition", "1s");
     }
-    if($(document).scrollTop() > 3600){
+    if($(document).scrollTop() > 2600){
       main.css("background-color", "#222");
       main.css("color", "#f2f2f2");
     }
-    if ($(document).scrollTop() > 3650) {
+    if ($(document).scrollTop() > 2650) {
       contact.css("opacity", 1);
       contact.css("transition", "1s");
       contact_title.css("transform", "scale(1)");
       contact_title.css("transition", "1s");
     }
-    if ($(document).scrollTop() > 3690) {
+    if ($(document).scrollTop() > 2690) {
       first_name.css("left", 0); 
       first_name.css("transition", "2s");
       last_name.css("right", 0);
