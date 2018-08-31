a81954ab4da0086c32003b4d31098ade2a6ef832
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-08-31 00:59:48 -0300

    alterando animação do form rm portrait

--- js/main.js
+++ js/main.js
@@ -124,17 +124,17 @@ if(height > width){
       project_area.css("opacity", 1);
       project_area.css("transition", "1s");
     }
-    if($(document).scrollTop() > 3250){
+    if($(document).scrollTop() > 4000){
       main.css("background-color", "#222");
       main.css("color", "#f2f2f2");
     }
-    if ($(document).scrollTop() > 3270) {
+    if ($(document).scrollTop() > 4010) {
       contact.css("opacity", 1);
       contact.css("transition", "1s");
       contact_title.css("transform", "scale(1)");
       contact_title.css("transition", "1s");
     }
-    if ($(document).scrollTop() > 3310) {
+    if ($(document).scrollTop() > 4020) {
      first_name.css("left", 0); 
      first_name.css("transition", "2s");
      last_name.css("right", 0);
