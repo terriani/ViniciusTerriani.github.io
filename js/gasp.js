f2b6f32b9d0fb46489d6ebc8fec47c1e707dd884
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-09-03 19:58:50 -0300

    alteração no codigo jquery para o controle de altura da div na tla e implementação da tla de boas vindas

--- js/gasp.js
+++ js/gasp.js
@@ -1,19 +1,42 @@
 
+var w_text1 = $(".t1");
+var w_text2 = $(".t2");
+var w_text3 = $(".t3");
+var w_text4 = $(".t4");
+var w_text5 = $(".t5");
+var dow = $(".btn-down");
+    
+w_text1.css("opacity", 0);
+w_text2.css("opacity", 0);
+w_text3.css("opacity", 0);
+w_text4.css("opacity", 0);
+w_text5.css("opacity", 0);
+dow.css("opacity", 0);
 
-//variaveis em jquery
-var intro_img = $(".intro-img");
-var text_intro = $(".text-intro");
-var title = $(".title");
+var obj = [w_text1, w_text2, w_text3, w_text4, w_text5];
 
-TweenMax.to(intro_img, 1.5, {
-    left: 0,
+
+TweenMax.staggerFromTo(obj, 3, {
+    scale: 0, 
+}, {
     opacity: 1,
-});
-TweenMax.to(text_intro,1.2, {
-    right: 0,
-    opacity: 1
-});
-TweenMax.to(title,2, {
-    left: 0,
-    opacity: 1
-});
+}, 0.9);
+
+TweenMax.staggerTo(obj, 1.3, {
+    opacity: 0,
+    color: "#ffca28",
+}, 2);
+
+TweenMax.to(dow, 1, {
+    opacity: 1,
+    delay: 9,
+    ease: Bounce.easeOut, y: -80,
+    
+})
+
+TweenMax.to(dow, 3, {
+    delay: 9,
+    ease: Bounce.easeOut, y: -30,
+    repeat: -1,
+    yoyo: true
+})
