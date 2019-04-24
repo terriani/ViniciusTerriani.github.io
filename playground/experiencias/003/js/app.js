141457ad2e747ce8c23abdcd6af3733249379d51
Vinicius Terriani <viniterriani.vt@gmail.com>
2019-04-23 23:05:39 -0300

    criaçao do sistema de navegaçao entre paginas

--- playground/experiencias/003/js/app.js
+++ playground/experiencias/003/js/app.js
@@ -6,6 +6,52 @@ $(".icon-menu").click(function (){
     $(".bar2").toggleClass("bar2-on");
 });
 
-function close(){
+$(".home").click(function(){
     $("nav").toggleClass("nav-on");
-}
+    $(".bar1").toggleClass("bar1-on");
+    $(".bar2").toggleClass("bar2-on");
+    $("#enterprise").removeClass("enterprise-on");
+    $("#portifolio").removeClass("portifolio-on");
+    $("#galery").removeClass("galery-on");
+    $("#contact").removeClass("contact-on");
+});
+
+$(".enterprise").click(function(){
+    $("#enterprise").toggleClass("enterprise-on");
+    $("nav").removeClass("nav-on");
+    $("#portifolio").removeClass("portifolio-on");
+    $("#galery").removeClass("galery-on");
+    $("#contact").removeClass("contact-on");
+    $(".bar1").toggleClass("bar1-on");
+    $(".bar2").toggleClass("bar2-on");
+});
+
+$(".portifolio").click(function(){
+    $("#portifolio").toggleClass("portifolio-on");
+    $("nav").removeClass("nav-on");
+    $("#enterprise").removeClass("enterprise-on");
+    $("#galery").removeClass("galery-on");
+    $("#contact").removeClass("contact-on");
+    $(".bar1").toggleClass("bar1-on");
+    $(".bar2").toggleClass("bar2-on");
+});
+
+$(".galery").click(function(){
+    $("#galery").toggleClass("galery-on");
+    $("nav").removeClass("nav-on");
+    $("#enterprise").removeClass("enterprise-on");
+    $("#portifolio").removeClass("portifolio-on");
+    $("#contact").removeClass("contact-on");
+    $(".bar1").toggleClass("bar1-on");
+    $(".bar2").toggleClass("bar2-on");
+});
+
+
+$(".contact").click(function(){
+    $("#contact").toggleClass("contact-on");
+    $("nav").removeClass("nav-on");
+    $("#enterprise").removeClass("enterprise-on");
+    $("#portifolio").removeClass("portifolio-on");
+    $(".bar1").toggleClass("bar1-on");
+    $(".bar2").toggleClass("bar2-on");
+});
